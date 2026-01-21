f99cbd2743c7b1d2e5428af6382d3a138865b757
Jonathan Marc Bearak <jbearak@guttmacher.org>
2026-01-21 16:27:47 -0500

    Remove unused description parameter from test helper

--- tests/unit/definition-extended-macro-context.test.ts
+++ tests/unit/definition-extended-macro-context.test.ts
@@ -36,8 +36,7 @@ describe('DefinitionProvider - Extended Macro Context Detection', () => {
         content: string,
         line: number,
         character: number,
-        expected: boolean,
-        description: string
+        expected: boolean
     ) => {
         const document = create_test_document(content);
         const position: Position = { line, character };
@@ -50,178 +49,83 @@ describe('DefinitionProvider - Extended Macro Context Detection', () => {
 
     describe('Positive cases - should detect extended macro context', () => {
         test('local macro with list function', () => {
-            test_context_detection(
-                'local x : list a',
-                0, 14, // position at 'a'
-                true,
-                'local x : list a'
-            );
+            test_context_detection('local x : list a', 0, 14, true);
         });
 
         test('local macro with list function and pipe operator', () => {
-            test_context_detection(
-                'local x : list a | b',
-                0, 16, // position at 'a'
-                true,
-                'local x : list a | b'
-            );
+            test_context_detection('local x : list a | b', 0, 16, true);
         });
 
         test('global macro with list function', () => {
-            test_context_detection(
-                'global y : list c',
-                0, 16, // position at 'c'
-                true,
-                'global y : list c'
-            );
+            test_context_detection('global y : list c', 0, 16, true);
         });
 
         test('local macro with word function', () => {
-            test_context_detection(
-                'local x : word 1 of a',
-                0, 20, // position at 'a'
-                true,
-                'local x : word 1 of a'
-            );
+            test_context_detection('local x : word 1 of a', 0, 20, true);
         });
 
         test('global macro with word function', () => {
-            test_context_detection(
-                'global y : word 2 of b',
-                0, 21, // position at 'b'
-                true,
-                'global y : word 2 of b'
-            );
+            test_context_detection('global y : word 2 of b', 0, 21, true);
         });
 
         test('local macro with piece function', () => {
-            test_context_detection(
-                'local x : piece 1 3 of a',
-                0, 23, // position at 'a'
-                true,
-                'local x : piece 1 3 of a'
-            );
+            test_context_detection('local x : piece 1 3 of a', 0, 23, true);
         });
 
         test('global macro with piece function', () => {
-            test_context_detection(
-                'global y : piece 2 4 of b',
-                0, 24, // position at 'b'
-                true,
-                'global y : piece 2 4 of b'
-            );
+            test_context_detection('global y : piece 2 4 of b', 0, 24, true);
         });
 
         test('with leading whitespace', () => {
-            test_context_detection(
-                '    local x : list a',
-                0, 18, // position at 'a'
-                true,
-                'with leading whitespace'
-            );
+            test_context_detection('    local x : list a', 0, 18, true);
         });
 
         test('with extra spaces around colon', () => {
-            test_context_detection(
-                'local x  :  list a',
-                0, 17, // position at 'a'
-                true,
-                'with extra spaces around colon'
-            );
+            test_context_detection('local x  :  list a', 0, 17, true);
         });
     });
 
     describe('Negative cases - should not detect extended macro context', () => {
         test('regular local assignment', () => {
-            test_context_detection(
-                'local x = 5',
-                0, 10, // position at '5'
-                false,
-                'local x = 5'
-            );
+            test_context_detection('local x = 5', 0, 10, false);
         });
 
         test('tab command with variable', () => {
-            test_context_detection(
-                'tab varname',
-                0, 4, // position at 'varname'
-                false,
-                'tab varname'
-            );
+            test_context_detection('tab varname', 0, 4, false);
         });
 
         test('display command', () => {
-            test_context_detection(
-                'display "hello"',
-                0, 8, // position at '"hello"'
-                false,
-                'display "hello"'
-            );
+            test_context_detection('display "hello"', 0, 8, false);
         });
 
         test('local without colon syntax', () => {
-            test_context_detection(
-                'local myvar something',
-                0, 12, // position at 'something'
-                false,
-                'local myvar something'
-            );
+            test_context_detection('local myvar something', 0, 12, false);
         });
 
         test('non-list extended function', () => {
-            test_context_detection(
-                'local x : display "test"',
-                0, 18, // position at '"test"'
-                false,
-                'local x : display "test"'
-            );
+            test_context_detection('local x : display "test"', 0, 18, false);
         });
 
         test('comment line', () => {
-            test_context_detection(
-                '* local x : list a',
-                0, 17, // position at 'a'
-                false,
-                'comment line'
-            );
+            test_context_detection('* local x : list a', 0, 17, false);
         });
 
         test('position before list keyword', () => {
-            test_context_detection(
-                'local x : list a',
-                0, 8, // position at ':'
-                false,
-                'position before list keyword'
-            );
+            test_context_detection('local x : list a', 0, 8, false);
         });
     });
 
     describe('Edge cases', () => {
         test('empty line', () => {
-            test_context_detection(
-                '',
-                0, 0,
-                false,
-                'empty line'
-            );
+            test_context_detection('', 0, 0, false);
         });
 
         test('position at end of line', () => {
-            test_context_detection(
-                'local x : list a',
-                0, 15, // position at end
-                true,
-                'position at end of line'
-            );
+            test_context_detection('local x : list a', 0, 15, true);
         });
 
         test('uppercase keywords should NOT match (Stata is case-sensitive)', () => {
-            test_context_detection(
-                'LOCAL x : LIST a',
-                0, 15, // position at 'a'
-                false,
-                'uppercase keywords should not match'
-            );
+            test_context_detection('LOCAL x : LIST a', 0, 15, false);
         });
     });
 });
